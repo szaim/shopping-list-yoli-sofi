2c644a3ccbb28d8190b7ea0234de0f97114b25d3
yolixtly <yolixtly_13@hotmail.com>
2016-07-19 12:48:55 -0400

    working on delete button functionality

--- app.js
+++ app.js
@@ -18,19 +18,36 @@
 
 
 $(document).ready(function() {
-	
+
 	//Fetching elements from HTML
+	var buttonWrapper = '<div class="shopping-item-controls">';
+	var checkedButton = '<button class="shopping-item-toggle"><span class="button-label"> Check </span></button>';
+	var deleteButton = '<button class="shopping-item-delete"> <span class="button-label">delete</span></button>';
+	
+	
 	function main() {
 		var form = $("#js-shopping-list-form");
 			form.submit(function(e) {
 				var input = $("#shopping-list-entry").val();
-				$(".shopping-list").append('<li>' + input + '</li>');
+
+				$(".shopping-list").append('<li><span class="shopping-item">' + input + '</span>' + buttonWrapper + checkedButton + " " + deleteButton + '</div></li>');
 				e.preventDefault();
 			});
 	}
 	main();
+	deleteItem();
+	
+function deleteItem() {
+	$('#shopping-list').on('click', '.shopping-item-delete', function(){
+		$(this).closest('li').hide();
+	})
+}
 		
 }); 
+
+
+
+
 /**
 another way to prevent default from function:
 
