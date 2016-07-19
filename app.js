710c3b434cf89427bc2c96fed5a2610f8fc62204
yolixtly <yolixtly_13@hotmail.com>
2016-07-19 11:40:27 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+// 1.add a new product 
+	/*
+		$('input').value();
+
+	*/
+
+//add a button that checks completed Items 
+//add a button that uncheked items
+	// input tag type checked 
+	// button 
+	//directly click on the item (double click )
+
+
+//add a button that deletes items 
+
+
+// add Date 
+
+
+$(document).ready(function() {
+	
+	//fetching the input
+		var newItem = $('#shopping-list-entry').val();
+
+		function collectItem () {
+			
+			alert(newItem);
+		}
+	
+});
